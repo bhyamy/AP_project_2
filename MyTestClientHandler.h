dcc1d794c699da6269c8f50d4f177df585a8c94e
bhyamy <bhyamy@gmail.com>
2020-01-21 19:25:50 +0200

    test client handler added.

--- MyTestClientHandler.h
+++ MyTestClientHandler.h
@@ -4,12 +4,33 @@
 
 #ifndef AP_PROJECT2_MYTESTCLIENTHANDLER_H
 #define AP_PROJECT2_MYTESTCLIENTHANDLER_H
+
 #include "ClientHandler.h"
+#include "StringReverser.h"
+#include "FileCacheManager.h"
 #include "Solver.h"
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <netinet/in.h>
+
+using namespace std;
 
 class MyTestClientHandler : public ClientHandler {
+
+    Solver<string, string>* _solver;
+    CacheManager<string, string>* _cacheManager;
+
+    //private methods
+    string readData(int client_socket);
+
+public:
+    //ctor
     MyTestClientHandler();
-    Solver* solver;
+    //dtor
+    virtual ~MyTestClientHandler();
+    //implemented functions
+    void handleClient(int client_socket) override;
 };
 
 
